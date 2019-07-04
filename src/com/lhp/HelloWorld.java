ee56900636d6d293db8b584aada8cbb96cd6c871
luoha <luohaiping0406@163.com>
2019-07-04 22:30:18 +0800

    Initial commit

---
+++ src/com/lhp/HelloWorld.java
@@ -0,0 +1,25 @@
+package com.lhp;
+
+public class HelloWorld {
+
+    /*public static void main(String[] args){
+        System.out.println("asdfasdfdsf");
+        System.out.println("asgard's");
+
+        for (int i = 0; i < 8; i++) {
+
+        }
+
+    }*/
+
+    public static void main(String[] args) {
+        System.out.println();
+        System.out.println();
+        System.out.println("args = [" + args + "]");
+        System.out.println("args = " + args);
+
+
+        HelloWorld hell = new HelloWorld();
+
+    }
+}
---
+++ web01/web/index.jsp
@@ -0,0 +1,17 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: luoha
+  Date: 2019/7/3
+  Time: 23:37
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>$Title$我的idea</title>
+  </head>
+
+  <body>
+    <h1 style="color:royalblue">klasdjhfljsdf</h1>
+  </body>
+</html>
